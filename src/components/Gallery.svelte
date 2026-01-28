e9f8833a56c1d67d9ebfede852cdd7caf903fc01
Dominik Bláha <jsem@dominikblaha.cz>
2026-01-28 14:32:46 +0100

    Fix gallery grid

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -2,8 +2,8 @@
   import { CldImage, configureCloudinary } from "svelte-cloudinary";
   import { cn } from "../utils/cn";
   import {
-    GalleryImage,
     GalleryThumbnail,
+    GalleryImage,
     LightboxGallery,
   } from "svelte-lightbox";
 
@@ -37,40 +37,41 @@
 </script>
 
 {#if imagePublicIds.length > 0}
-  <div class="grid grid-cols-2 md:grid-cols-4 gap-4" id={galleryId}>
-    <LightboxGallery >
+  <div class="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-3" id={galleryId}>
+    <LightboxGallery>
       <svelte:fragment slot="thumbnail">
         {#each imagePublicIds as publicId, index (publicId)}
-          <div
+          <GalleryThumbnail
+            id={index}
             class={cn(
-              "relative glightbox rounded-lg overflow-hidden cursor-pointer hover:opacity-90 transition-opacity",
-              index === 0 && "col-start-2 col-span-2 row-span-2",
-              index === 1 && "col-start-1 w-2/3 mt-auto ml-auto",
-              index === 2 && "col-start-1",
-              isHigher(index, [0, 6, 14], 4) && "col-span-2 row-span-2",
-              isHigher(index, [0, 6, 9, 12, 14]) && "row-span-2",
+              "relative glightbox rounded-lg overflow-hidden cursor-pointer hover:opacity-90 transition-opacity max-md:aspect-3/2",
+              index === 0 && "md:col-start-2 md:col-span-2 md:row-span-2",
+              index === 1 && "md:col-start-1 md:w-2/3 md:mt-auto md:ml-auto",
+              index === 2 && "md:col-start-1",
+              isHigher(index, [0, 6, 14], 4) && "md:col-span-2 md:row-span-2",
+              isHigher(index, [0, 6, 9, 12, 14]) && "md:row-span-2",
+              isHigher(index, [0, 5]) && "max-md:col-span-2",
             )}
           >
-            <GalleryThumbnail id={index}>
-              <CldImage
-                src={publicId}
-                width={1088}
-                height={725}
-                alt=""
-                loading="lazy"
-                crop="fill"
-                sizes="(max-width: 640px) 50vw, 20vw"
-                class={cn(
-                  "w-full h-full object-cover relative z-10 transition-opacity duration-300",
-                  loadedImages.has(index) ? "opacity-100" : "opacity-0",
-                )}
-                data-image-index={index}
-                onload={() => {
-                  handleImageLoad(index);
-                }}
-              />
-            </GalleryThumbnail>
-          </div>
+            <CldImage
+              src={publicId}
+              width={2400}
+              height={1600}
+              alt=""
+              loading="lazy"
+              sizes="(max-width: 640px) 50vw, 20vw"
+              objectFit="cover"
+              crop="auto"
+              class={cn(
+                "size-full! max-w-full max-h-full object-cover relative z-10 transition-opacity duration-300",
+                loadedImages.has(index) ? "opacity-100" : "opacity-0",
+              )}
+              data-image-index={index}
+              onload={() => {
+                handleImageLoad(index);
+              }}
+            />
+          </GalleryThumbnail>
         {/each}
       </svelte:fragment>
       {#each imagePublicIds as publicId, index (publicId)}
